bc1e0c14a37a0062034a56f97dba95bdaad9ee5d
Justin Tran <jhtran023@gmail.com>
2024-06-01 12:13:28 +1000

    Add tower level functionality
    Remove tower collider

--- Assets/Scripts/Player/UIPlayer.cs
+++ Assets/Scripts/Player/UIPlayer.cs
@@ -9,6 +9,7 @@ public class UIPlayer : MonoBehaviour
     [SerializeField] TextMeshProUGUI shield;
     [SerializeField] TextMeshProUGUI level;
     [SerializeField] TextMeshProUGUI experience;
+    [SerializeField] TextMeshProUGUI LevelUpTowerText;
 
     [SerializeField] PlayerScript player;
     [SerializeField] TowerManager towerManager;
@@ -16,11 +17,16 @@ public class UIPlayer : MonoBehaviour
     [SerializeField] GameObject PlayerLevelUI;
     [SerializeField] GameObject ChoosingTowerUI;
 
+    private Camera mainCamera;
+    private bool isLeveling = false;
+
     // Start is called before the first frame update
     void Start()
     {
         PlayerLevelUI.SetActive(false);
         ChoosingTowerUI.SetActive(false);
+        SetLevelingTowerState(false);
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
@@ -30,24 +36,57 @@ public class UIPlayer : MonoBehaviour
         shield.text = "Shield: " + player.Shield;
         level.text = "Lvl: " + player.Level;
         experience.text = "Exp: " + player.Experience + "/" + player.MaxExperience;
+        if (isLeveling)
+        {
+            HandleLevelTower();
+        }
     }
 
+    private void HandleLevelTower()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+            if (hit.collider != null)
+            {
+                GameObject hitObject = hit.collider.gameObject;
+                if (hitObject.CompareTag("Tower"))
+                {
+                    BaseTower baseTower = hitObject.GetComponent<BaseTower>();
+                    if (baseTower != null)
+                    {
+                        baseTower.LevelTower();
+                        SetLevelingTowerState(false);
+                        Time.timeScale = 1f;
+                    }
+                }
+            }
+        }
+    }
+
+    private void SetLevelingTowerState(bool state)
+    {
+        isLeveling = state;
+        LevelUpTowerText.enabled = state;
+    }
+
+
+    // Button functions
     public void PlayerLevelUpUI()
     {
         PlayerLevelUI.SetActive(true);
     }
-
     public void ActivateChooseTowerUI()
     {
         PlayerLevelUI.SetActive(false);
         ChoosingTowerUI.SetActive(true);
     }
-
     public void LevelUpTower()
     {
-        Time.timeScale = 1;
+        SetLevelingTowerState(true);
+        PlayerLevelUI.SetActive(false);
     }
-
     public void PlaceGenericTowerButton()
     {
         towerManager.CreateGenericTower();
